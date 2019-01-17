7647fe3f987183d3952a5d964f3f89196e0f68d4
kamiyechung <46262732+kamiyechung@users.noreply.github.com>
2019-01-17 22:41:09 +0100

    Initial commit

---
+++ mairies.rb
@@ -0,0 +1,38 @@
+require 'nokogiri'
+require 'rubygems'
+require 'open-uri'
+require 'pry'
+
+
+#Récupération d'une array avec la liste des villes
+def get_townhall_urls
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/val-d-oise.html"))
+  page.xpath('//a[@class = "lientxt"]').map { |town|
+  town.text }
+end
+
+#Récupération d'une array avec la liste des emails associés à chaque ville
+def get_townhall_email
+  page = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/"))
+  news_links = page.css("a")
+  news_links.map { |link|
+  page_annuaire = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/#{link['href']}"))
+  page_annuaire.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]').map { |mail|
+  mail.text }}
+end
+
+# Fusion des 2 arrays dans une hash
+def megahash
+arraytamere = []
+megarray = get_townhall_urls.zip(get_townhall_email.flatten)
+megaarrays = megarray.map { |e| e.delete('$').to_f }
+final_result_hash_a = megarray.map { |town, email| Hash["#{town}": email] }
+arraytamere << final_result_hash_a
+
+end
+
+def perform
+    megahash
+end
+
+perform
